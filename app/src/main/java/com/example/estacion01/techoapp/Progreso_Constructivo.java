b9f918b7260e380c2632abe01f1fd14364877df5
najiro8v <harolantonio77@outlook.com>
2019-01-16 17:13:19 -0600

    ya hay mas de una pantalla o activity

---
+++ app/src/main/java/com/example/estacion01/techoapp/Progreso_Constructivo.java
@@ -0,0 +1,13 @@
+package com.example.estacion01.techoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Progreso_Constructivo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progreso__constructivo);
+    }
+}

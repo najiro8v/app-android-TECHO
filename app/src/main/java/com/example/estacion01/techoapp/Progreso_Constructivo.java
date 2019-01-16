7a4390312b9121ce6bfc7112379e2acff6ee6cc0
najiro8v <harolantonio77@outlook.com>
2019-01-16 17:35:48 -0600

    ya se puede hacer transferencia entre ellos

--- app/src/main/java/com/example/estacion01/techoapp/Progreso_Constructivo.java
+++ app/src/main/java/com/example/estacion01/techoapp/Progreso_Constructivo.java
@@ -1,7 +1,9 @@
 package com.example.estacion01.techoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Progreso_Constructivo extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Progreso_Constructivo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progreso__constructivo);
+    }
+    public void change(View view)
+    {
+        Intent siguiente=new Intent(this,TECHO.class);
+        startActivity(siguiente);
     }
 }
--- app/src/main/java/com/example/estacion01/techoapp/TECHO.java
+++ app/src/main/java/com/example/estacion01/techoapp/TECHO.java
@@ -1,7 +1,9 @@
 package com.example.estacion01.techoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TECHO extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TECHO extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_techo);
+    }
+    public void change(View view)
+    {
+        Intent siguiente=new Intent(this,Progreso_Constructivo.class);
+        startActivity(siguiente);
     }
 }

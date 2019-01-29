dbd532e5188d05b3c147ec9ccf62e3e4d833af0f
najiro8v <harolantonio77@outlook.com>
2019-01-28 18:06:12 -0600

    caudricula para calculadora

--- app/src/main/java/com/example/estacion01/techoapp/Nivel.java
+++ app/src/main/java/com/example/estacion01/techoapp/Nivel.java
@@ -1,7 +1,9 @@
 package com.example.estacion01.techoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Nivel extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Nivel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nivel);
+    }
+    public void change(View view)
+    {
+        Intent siguiente=new Intent(this,TECHO.class);
+        startActivity(siguiente);
     }
 }

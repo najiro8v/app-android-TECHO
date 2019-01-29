113f37e02988c42d158c44182bbbf6899adacd74
najiro8v <harolantonio77@outlook.com>
2019-01-29 15:08:18 -0600

    me desisti del seekbar

--- app/src/main/java/com/example/estacion01/techoapp/Calculadora.java
+++ app/src/main/java/com/example/estacion01/techoapp/Calculadora.java
@@ -1,5 +1,6 @@
 package com.example.estacion01.techoapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,49 +8,32 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
 
 
 public class Calculadora extends AppCompatActivity {
-    private TextView et;
-    private SeekBar s;
+    private EditText et;
+    private   SeekBar s;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
-        tranf();
-        et=(TextView)findViewById(R.id.texto);
+        et=(EditText)findViewById(R.id.texto);
         s=(SeekBar)findViewById(R.id.seekBar);
+        //et.setText(s.getMax());
+        s.setProgress(5);
+        s.setOnSeekBarChangeListener(this);
+       // tranf();
     }
-    public void change(View view)
-    {
+    public void test(View view){
+        s.setProgress(s.getProgress()+5);
+    }
+    public void change(View view) {
         Intent siguiente=new Intent(this,TECHO.class);
-
         startActivity(siguiente);
     }
-    public void tranf(){
-     et.setText(s.getProgress());
-     s.setOnSeekBarChangeListener(
-             new SeekBar.OnSeekBarChangeListener(){
-                 int progreso;
-                 public void onProgressChanged(SeekBar bara,int progress, boolean  fromuser){
-                     progreso=progress;
-                     et.setText(progreso);
 
 
-                 }
 
-                 @Override
-                 public void onStartTrackingTouch(SeekBar seekBar) {
-
-                 }
-
-                 @Override
-                 public void onStopTrackingTouch(SeekBar seekBar) {
-
-                 }
-
-             }
-     );
-
-    }
 }
